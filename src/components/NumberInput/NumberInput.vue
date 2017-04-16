e2cc75a5ca70b1782bea358e50ab91243c899d92
huangsi <huangsijs@gmail.com>
2017-04-16 22:49:36 +0800

    add number input

--- src/components/NumberInput/NumberInput.vue
+++ src/components/NumberInput/NumberInput.vue
@@ -1,12 +1,24 @@
 <template>
-	<input 
-		type='number' 
-		:value='currValue' 
-		:min='min'
-		:max='max'
-		:step='step'
-		@change='handleChange'
-	/>
+	<div>
+		<div
+			@click='handleDown'
+			@focus='handleFocus'
+			tabIndex='0'
+		>-</div>
+		<input 
+			type='number' 
+			:value='currValue' 
+			:min='min'
+			:max='max'
+			:step='step'
+			@change='handleChange'
+			@blur='handleBlur'
+			:placeHolder='placeHolder'
+		/>
+		<div
+			@click='handleUp'
+		>+</div>
+	</div>
 </template>
 
 <script>
@@ -18,24 +30,92 @@ export default {
 			type: Number,
 			default: 0
 		},
-		min: Number,
-		max: Number,
+		min: {
+			type: Number,
+			default: Infinity
+		},
+		max: { 
+			type: Number,
+			default: -Infinity
+		},
 		step: {
 			type: Number,
 			default: 1
+		},
+		fixed: {
+			type: Number,
+			default: 0
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		},
+		placeHolder: {
+			type: String,
+			default: '请输入数字'
 		}
 	},
 
 	data() {
 		return {
-			currValue: null,
-			blurValue: null
+			currValue: 0,
+			blurValue: null,
+			upDisabled: false,
+			downDisabled: false
 		}
 	},
 
 	methods: {
 		handleChange(e) {
-			console.log(e.target.value)
+			const v = Number(e.target.value)
+			if(isNaN(v)) {
+				return false
+			}
+			this.blurValue = this.fix(e.target.value)
+		},
+		handleUp() {
+			const v = this.fix(this.currValue) + 1
+			const max = this.max
+			if(v >= max) {
+				v = max
+				this.upDisabled = true 
+			} else {
+				this.upDisabled = false
+			}
+			this.currValue = v
+
+		},
+		handleDown() {
+			const v = this.fix(this.currValue) - 1
+			const min = this.min
+			if(v <= min) {
+				v = min 
+				this.downDisabled = true
+			} else {
+				this.downDisabled = false
+			}
+			this.currValue = v
+		},
+		handleFocus() {
+			console.log('foc')
+		},
+		handleBlur() {
+			this.currValue = this.blurValue
+		},
+		fix(v) {
+			return Number(Number(v).toFixed(this.fixed)) 
+		},
+		changeValue (v) {
+		
+		}
+	},
+
+	watch: {
+		value (v) {
+			this.currentValue = v
+		},
+		currentValue (v) {
+			this.changValue(v)
 		}
 	}
 }
